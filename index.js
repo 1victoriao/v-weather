8c616b379faecc5af2deeb42279e79b44ef186f3
1victoriao <victoria.ona.anderson@gmail.com>
2024-07-05 12:08:08 -0500

    Added days of the week function and some css

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ function searchCity(city) {
   axios.get(apiURL).then(refreshWeather);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -58,27 +65,31 @@ function getForecast(city) {
   let apiKey = `0ao0eaf24fd28ada4f74eb3d2b5ect9b`;
   let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
 
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(apiURL).then(displayForecast);
 }
 
 function displayForecast(response) {
-
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌤️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div > <img src="${
+      day.condition.icon_url
+    }" class="weather-forecast-icon"/></div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
-        <strong>87°</strong>
+        <strong>${Math.round(day.temperature.maximum)}°</strong>
       </div>
-      <div class="weather-forecast-temperature">79°</div>
+      <div class="weather-forecast-temperature">${Math.round(
+        day.temperature.minimum
+      )}°</div>
     </div>
   </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -89,5 +100,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Baton Rouge");
-
-
